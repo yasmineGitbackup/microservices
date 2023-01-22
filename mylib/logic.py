a257345673efcaed88cc0ef262bd418d01da6498
Yasmine <65870261+phoebe20200523@users.noreply.github.com>
2023-01-22 03:59:32 +0000

    adding fastapi

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,7 +1,6 @@
 import wikipedia
 
-# from textblob import TextBlob
-
+from textblob import TextBlob
 
 def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
@@ -16,10 +15,9 @@ def search_wiki(name):
     results = wikipedia.search(name)
     return results
 
+def phrase(name):
+    """Returns phrases from wikipedia"""
 
-# def phrase(name):
-#    """Returns phrases from wikipedia"""
-
-#    page = wiki(name)
-#    blob = TextBlob(page)
-#    return blob.noun_phrases
+    page = wiki(name)
+    blob = TextBlob(page)
+    return blob.noun_phrases
